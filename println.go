d5bc321c0f5060993a70959e34f9bb263d3ded8a
J3ssh <87490958+J3ssh@users.noreply.github.com>
2021-07-15 19:51:53 +0100

    Create println.go
    Hello World from the GoLang playground

---
+++ println.go
@@ -0,0 +1,7 @@
+package main //Main package
+
+import "fmt" // importing format package, this is the best way for single imports
+
+func main () { //function called main func name 
+  fmt.Println ("Hello World") //Println
+  }//end main function 
