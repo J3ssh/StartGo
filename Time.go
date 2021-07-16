97345a2a7e745c1003e17287f2636310cd25c81e
J3ssh <87490958+J3ssh@users.noreply.github.com>
2021-07-16 13:16:28 +0100

    Create Time.go
    GoLang playground print Time

---
+++ Time.go
@@ -0,0 +1,15 @@
+package main //Main package 
+
+import (
+  
+  "fmt" //Importing Format
+  "time" //Importing Time 
+)
+
+func main () { //Main function
+  fmt.Println("Current Time", time.Now()) //Prints current time  
+  
+  
+}
+  
+  
