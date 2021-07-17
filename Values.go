5a857529831ab7bc2abc3bdc9bd838bdb4df0c4d
J3ssh <87490958+J3ssh@users.noreply.github.com>
2021-07-17 18:18:56 +0100

    Create Values.go
    Go by Example
    markmcgranaghan.com

---
+++ Values.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() { //Research if the first function always has to be named main?
+	//print two strings
+	fmt.Println("go" + "lang")
+	//Maths
+	fmt.Println("1+1", 1+1)
+	fmt.Println("7.0/3.0", 7.0/3.0)
+	//boolean
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
