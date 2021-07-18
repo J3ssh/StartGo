ee55b4afcb0ab4b66de7b0e360b0f089cd3bbbc1
J3ssh <87490958+J3ssh@users.noreply.github.com>
2021-07-18 17:45:53 +0100

    Create Variables.go
    Go by Example: Variables

---
+++ Variables.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+  
+  var a = "first" //string 
+  fmt.Println(a)
+  
+  var b, c int = 1, 2 //interger 
+  fmt.Println(b, c)
+  
+  var e = true //boolean
+  fmt.Println(e)
+  
+  f := "apple" // Shorter way to declare a variable
+  
+  var 
