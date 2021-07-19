6c6e7ae43a77f13387f135f8fcce3f45e9abb459
J3ssh <87490958+J3ssh@users.noreply.github.com>
2021-07-19 17:28:27 +0100

    Create SimpleWeb.go
    A basic web server using GoLang to display text running on port 8000.

---
+++ SimpleServices/SimpleWeb.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	//First basic GoLang webserver
+
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Hello, World!") //displays on the page /hello
+
+	}
+
+	http.HandleFunc("/hello", helloHandler)
+	log.Println("Listing for requests at http://localhost:8000/hello")
+	log.Fatal(http.ListenAndServe(":8000", nil))
+
+}
